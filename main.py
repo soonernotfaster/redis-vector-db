9ddcc4c594af45222eca734bcbfc6df7b4469167
Steven Solomon <solomon.steven.m@gmail.com>
2025-03-04 16:12:59 -0500

    Read in file

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def main():
-    print("Hello from site-search!")
+import os
+from bs4 import BeautifulSoup
+
+FILENAME = "../soonernotfaster/content/posts/writing-maybe-in-ruby-lessons-learned.md"
+def main() -> None:
+    lines = None
+    abs_filepath = os.path.abspath(FILENAME)
+    with open(abs_filepath, "r") as f:
+        lines = f.readlines()
+
+    print(lines)
 
 
 if __name__ == "__main__":
